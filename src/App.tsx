232645a708e927bd1398fc701821a4e71358c169
Louis G <louisgustavo07@gmail.com>
2022-06-04 22:07:08 +0700

    edit `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const Navbar = React.lazy(
+  () => import('./components/Navbar')
+)
+
+const Home = React.lazy(
+  () => import('./components/Home')
+)
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='bg-primary'>
+      <div className="bg">
+        <Navbar />
+        <Home />
+      </div>
+    </div>
   );
 }
---
+++ src/components/Works.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Works() {
+    return (
+        <div className='container relative -top-10'>
+            <h2 className='text-5xl font-bold text-center'>Works</h2>
+        </div>
+    );
+  }
