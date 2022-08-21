4de56ad95f088cfdfef7e39219e4c222c7349bd2
Louis G <louisgustavo07@gmail.com>
2022-08-21 17:22:34 +0700

    chore: add dummy data

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -48,5 +48,15 @@ export default [
     techStacks: "HTML/CSS, JavaScript, React, Tailwind CSS, Figma",
     codeLink: "",
     liveDemoLink: ""
+  },
+  {
+    id: 5,
+    title: "Title",
+    categories: "Frontend, Backend, UI/UX",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus adipisci aliquam tempora omnis sint, beatae quas fugiat doloribus quasi magni ipsa sit delectus perspiciatis a explicabo, qui at? Ut, distinctio.",
+    image: "",
+    techStacks: "HTML/CSS, JavaScript, React, Tailwind CSS, Figma",
+    codeLink: "",
+    liveDemoLink: ""
   }
 ];
