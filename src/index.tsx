f4f81ead42f7ca9aee3d89879bff1303f0085f5c
Louis G <louisgustavo07@gmail.com>
2022-09-15 15:21:06 +0700

    feat: revert to `BrowserRouter`

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { App } from "./App";
 import ScrollToTop from "./components/ScrollToTop";
@@ -9,10 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <div className="bg-slate-200 text-slate-800 dark:bg-slate-900 dark:text-white">
     <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
         <ScrollToTop />
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </React.StrictMode>
   </div>
 );
