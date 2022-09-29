84539326608b351069b27ee64b203835fb61a2d6
Louis G <louisgustavo07@gmail.com>
2022-09-29 19:22:58 +0700

    feat: add gradient rotation

---
+++ src/components/HighlightText.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function HighlightText(props: { children: React.ReactNode }) {
+  const ref = useRef<HTMLLinkElement>(null);
+  const [degree, setDegree] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDegree(degree >= 360 ? 0 : degree + 15);
+      if (ref.current !== null) {
+        ref.current.style.backgroundImage = `linear-gradient(${degree}deg, var(--tw-gradient-stops))`;
+      }
+    }, 75);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <span
+      ref={ref}
+      className="from-fuchsia-700 to-blue-700 bg-clip-text text-transparent 
+     transition dark:from-fuchsia-400 dark:to-blue-400"
+    >
+      {props.children}
+    </span>
+  );
+}
--- src/containers/Hero.tsx
+++ src/containers/Hero.tsx
@@ -1,4 +1,5 @@
 import ArrowDownSLineIcon from "remixicon-react/ArrowDownSLineIcon";
+import HighlightText from "../components/HighlightText";
 import Button from "../components/Button";
 import SocialMediaLinks from "../components/SocialMediaLinks";
 
@@ -12,10 +13,8 @@ export default function Hero() {
       <div className="flex h-3/4 flex-col justify-center gap-6">
         <h2 className="text-2xl md:text-4xl">I'm Louis Gustavo</h2>
         <h1 className="break-words text-4xl font-extrabold tracking-tight xs:text-5xl sm:text-6xl lg:text-7xl">
-          A <span className="text-highlight-dark dark:text-highlight-light">software engineer</span>{" "}
-          and a{" "}
-          <span className="text-highlight-dark dark:text-highlight-light">Computer Science</span>{" "}
-          undergraduate.
+          A <HighlightText>software engineer</HighlightText> and a{" "}
+          <HighlightText>Computer Science</HighlightText> undergraduate.
         </h1>
         <SocialMediaLinks className="mt-3" />
       </div>
