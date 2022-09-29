dffdb0d5aea88a5e5378bef1ef04455fae5a4363
Louis G <louisgustavo07@gmail.com>
2022-09-29 23:48:55 +0700

    feat(about): paraphrase sentences

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -8,22 +8,22 @@ export default function About() {
     <section className="container md:max-w-screen-sm md:px-0">
       <Heading2>Hey, it's Louis "itslouisgs"—a software engineer based in Indonesia.</Heading2>
       <p className="text-muted-dark dark:text-muted">
-        I am fond of building stuffs on the computer and exploring new programming languages,
-        libraries, and frameworks. My interest in web development started when I was obssesed with
-        customizing my profile page's style with HTML and CSS on{" "}
-        <InlineLink href="https://www.fandom.com/">Fandom</InlineLink>.
+        I enjoy coding for things on the Internet and exploring new programming languages,
+        libraries, and frameworks. My obsession with modifying my{" "}
+        <InlineLink href="https://www.fandom.com/">Fandom</InlineLink> profile page's theme with
+        HTML and CSS sparked my interest in web development.
       </p>
       <p className="text-muted-dark dark:text-muted">
         I'm currently a third-year <b>computer science undergraduate</b> at{" "}
-        <InlineLink href="https://binus.ac.id">BINUS University</InlineLink> while serving as{" "}
+        <InlineLink href="https://binus.ac.id">BINUS University</InlineLink> and{" "}
         <b>Manager of Web Development Division</b> at{" "}
         <InlineLink href="https://himti.or.id">HIMTI BINUS University</InlineLink>. On top of that,
-        I received the Mentoring Scholarship Program at BINUS University for maintaining over 3.5
-        GPA, provided that I conduct peer-to-peer learning sessions for other students.
+        I was granted the Mentoring Scholarship Program at BINUS University for having a GPA of over
+        3.5, provided that I conduct peer-to-peer learning sessions for other students.
       </p>
       <Heading3 id="skills">Skills</Heading3>
       <p className="pb-3 text-muted-dark dark:text-muted">
-        Here are some technologies that I know:
+        Here are some technologies that I am familiar with:
       </p>
       <ul className="pb-4 text-muted-dark dark:text-muted md:pb-8">
         {skills.map((li, index) => (
@@ -37,12 +37,12 @@ export default function About() {
       </ul>
       <Heading3 id="contacts">Contact Me</Heading3>
       <p className="text-muted-dark dark:text-muted">
-        Whether you have questions or just wanna say "hi", you can email me at{" "}
+        Wanna ask me a question or simply say hi? You can reach me via email at{" "}
         <InlineLink href="mailto:louis.gustavo@binus.ac.id">louis.gustavo@binus.ac.id</InlineLink>.
-        I'd love to hear from you.
+        I'd be delighted to hear from you.
       </p>
       <p className="text-muted-dark dark:text-muted">
-        You can also stalk me on{" "}
+        Additionally, you may find me on{" "}
         <InlineLink href="https://github.com/itslouisgs">GitHub</InlineLink> and{" "}
         <InlineLink href="https://linkedin.com/in/louis-gustavo">LinkedIn</InlineLink>.
       </p>
