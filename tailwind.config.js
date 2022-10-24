988f9f65b5631914bb4a94965243cc7d4989d988
Louis G <louisgustavo07@gmail.com>
2022-10-24 20:56:54 +0700

    refactor: use tailwind utilities whenever possible

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
     screens: {
