339637280c9b0d0b1bc4a91bab35d2bba0c08e44
Louis Gustavo <louis.gustavo@gdn-commerce.com>
2024-02-13 16:25:37 +0700

    test again

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,11 @@ import checkDarkTheme from '@/utils/checkDarkTheme.ts'
 export default function App() {
   useEffect(() => {
     if (checkDarkTheme()) {
-      document.documentElement.classList.add('dark')
+      // document.documentElement.classList.add('dark')
       return
     }
-    document.documentElement.classList.remove('dark')
+    // document.documentElement.classList.remove('dark')
   }, [])
-
-  should be error
 
   return <Router />
 }
