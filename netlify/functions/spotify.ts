d47f1130749bce99524621a60b52fc86a639e5ee
Louis Gustavo <louisgustavo07@gmail.com>
2024-05-24 15:23:59 +0700

    feat: test spotify api using netlify functions

---
+++ netlify/functions/spotify.ts
@@ -0,0 +1,75 @@
+import type { Handler } from '@netlify/functions'
+
+const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID
+const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET
+const refresh_token = import.meta.env.VITE_SPOTIFY_REFRESH_TOKEN
+
+const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64')
+const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`
+
+const getAccessToken = async () => {
+  const params = new URLSearchParams()
+  params.append('grant_type', 'refresh_token')
+  params.append('refresh_token', refresh_token)
+
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${basic}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: params
+  })
+
+  const { access_token } = await response.json()
+  return access_token
+}
+
+export const getNowPlaying = async (): Promise<Response> => {
+  const access_token = await getAccessToken()
+
+  return fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`
+    }
+  }).then(res => res.json())
+}
+
+export const handler: Handler = async () => {
+  const response = await getNowPlaying()
+
+  let response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    }
+  }
+
+  if (
+    response.status === 204 ||
+    response.status > 400 ||
+    response.data.currently_playing_type !== 'track'
+  ) {
+    return {
+      ...response,
+      body: {
+        isPlaying: false
+      }
+    }
+  }
+
+  return {
+    ...response,
+    body: {
+      isPlaying: response.data.is_playing,
+      title: response.data.item.name,
+      album: response.data.item.album.name,
+      artist: response.data.item.album.artists.map(artist => artist.name).join(', '),
+      albumImageUrl: response.data.item.album.images[0].url,
+      songUrl: response.data.item.external_urls.spotify
+    }
+  }
+}
