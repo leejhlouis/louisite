095bb6305e18078a8e2b8e3bc945c93e42224dea
Louis Gustavo <louisgustavo07@gmail.com>
2024-06-03 20:08:02 +0700

    fix: button styling

--- src/components/common/NavLinks.tsx
+++ src/components/common/NavLinks.tsx
@@ -5,7 +5,7 @@ import navItems from '@/_data/navItems.ts'
 const PrimaryButton = lazy(() => import('@/components/common/reusable/button/PrimaryButton.tsx'))
 const Badge = lazy(() => import('@/components/common/reusable/Badge.tsx'))
 
-export default function NavLinks() {
+export default function NavLinks({ newBadge }: { newBadge: boolean }) {
   const links = navItems.map((item, index) => (
     <li
       className='flex'
@@ -17,7 +17,14 @@ export default function NavLinks() {
       >
         {({ isActive }) => <PrimaryButton active={isActive}>{item.name}</PrimaryButton>}
       </NavLink>
-      {item.isNew && <Badge className='ml-1' size='small'>NEW</Badge>}
+      {newBadge && item.isNew && (
+        <Badge
+          className='ml-1'
+          size='small'
+        >
+          NEW
+        </Badge>
+      )}
     </li>
   ))
 
